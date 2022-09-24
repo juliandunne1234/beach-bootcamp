da462fb96b5c468874cd67ee076b5c676acb3d5e
juliandunne1234 <julianmorandunne@gmail.com>
2022-09-24 13:51:26 +0000

    Added BootcampAdmin and SignUpAdmin classes to booking app

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Bootcamp, SignUp
 
-# Register your models here.
+
+@admin.register(Bootcamp)
+class BootcampAdmin(admin.ModelAdmin):
+    """
+    BootcampAdmin class
+    """
+    prepopulated_fields = {'slug': ('bootcamp_title',)}
+    list_display = ('bootcamp_title', 'slug', 'bootcamp_date', 'max_capacity')
+    list_filter = ('bootcamp_title', 'bootcamp_date')
+
+
+@admin.register(SignUp)
+class SignUpAdmin(admin.ModelAdmin):
+    """
+    SignUpAdmin class
+    """
+    list_display = ('full_name', 'user', 'bootcamp', 'signup_date')
+    list_filter = ('full_name', 'user', 'bootcamp', 'signup_date')
