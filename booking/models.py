652e61282bbffbfebd13139b77460f4763a34b6b
juliandunne1234 <julianmorandunne@gmail.com>
2022-09-24 12:01:17 +0000

    Added Bootcamp and SignUp models to booking app

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Bootcamp(models.Model):
+    """
+    Bootcamp model
+    """
+    bootcamp_title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    bootcamp_date = models.DateField()
+    max_capacity = models.IntegerField(default=12)
+
+    def __str__(self):
+        return self.bootcamp_title
+
+
+class SignUp(models.Model):
+    """
+    SignUp model
+    """
+    full_name = models.CharField(max_length=80)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="signed_up")
+    bootcamp = models.ForeignKey(Bootcamp, on_delete=models.CASCADE, related_name="bootcamps")
+    signup_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
