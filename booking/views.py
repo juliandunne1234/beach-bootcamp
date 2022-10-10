72eb466e88eff2828103e258972ae7bc0e5cf706
juliandunne1234 <julianmorandunne@gmail.com>
2022-10-10 09:21:34 +0000

    Updated booking views.py cap number of bookings and number of bootcamp places the user can book

--- booking/views.py
+++ booking/views.py
@@ -6,6 +6,23 @@ from .models import SignUp
 from .forms import SignUpForm
 
 
+def current_registrations(bootcamp_date):
+
+    current_registrations = len(SignUp.objects.filter(
+        bootcamp_date=bootcamp_date,)
+        )
+
+    return current_registrations
+
+
+def bootcamp_capacity(booking_confirmed):
+
+    max_capacity = 12
+    available_spaces = max_capacity - booking_confirmed
+
+    return available_spaces
+
+
 class BootcampRegistration(View):
 
     def get(self, request, *args, **kwargs):
@@ -27,18 +44,41 @@ class BootcampRegistration(View):
             bootcamp_date = request.POST.get('bootcamp_date')
             email = request.POST.get('email')
             queryset = SignUp.objects.filter(email=email, bootcamp_date=bootcamp_date)
-
+            booking_confirmed = current_registrations(bootcamp_date)
+            available_spaces = bootcamp_capacity(booking_confirmed)
+                        
             if  queryset:
                 messages.add_message(
-                        request, messages.WARNING,
-                        f"Hi {full_name}, you have already registered for the {bootcamp_date} bootcamp!")
+                        request, 
+                        messages.WARNING,
+                        f"Hi {full_name}, you have already registered for the {bootcamp_date} bootcamp!"
+                        )
+
+            elif available_spaces <= 0:
+                messages.add_message(
+                        request,
+                        messages.INFO,
+                        f"Hi {full_name}, sorry due to demand {bootcamp_date} bootcamp is fully booked! "
+                        f"Please select another bootcamp."
+                        )
 
             else:
                 bootcamp_registration_form.save()
                 messages.add_message(
-                        request, messages.SUCCESS,
-                        f"Hi {full_name}, thank you for registering for the {bootcamp_date} bootcamp!")
+                        request, 
+                        messages.SUCCESS,
+                        f"Hi {full_name}, thank you for registering for the {bootcamp_date} bootcamp!"
+                        )
 
             return HttpResponseRedirect(
                     reverse("index",)               
                 )
+
+        else:
+            return render(
+            request,
+            "registration.html",
+            {
+                "signup_form": SignUpForm()
+            }
+        )
