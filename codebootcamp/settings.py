c281f7b7fd9cdc1855439ab5397a64764f6cb5a9
juliandunne1234 <julianmorandunne@gmail.com>
2022-10-10 20:33:58 +0000

    Added timeout function to alert messages

--- codebootcamp/settings.py
+++ codebootcamp/settings.py
@@ -21,6 +21,8 @@ if os.path.isfile('env.py'):
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -28,7 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['beachbootcamp2.herokuapp.com', 'localhost']
 
